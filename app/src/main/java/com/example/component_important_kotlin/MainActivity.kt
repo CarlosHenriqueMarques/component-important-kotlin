244d2ad463c77f4f9661707babf918b1f718e955
Carlos Maia <carlos.marques89@gmail.com>
2019-12-01 10:39:30 -0500

    Primeiro component TOAST NOTIFICATION

--- app/src/main/java/com/example/component_important_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/component_important_kotlin/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.component_important_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonToastMe.setOnClickListener {
+            Toast.makeText(this,"Toast Notification",Toast.LENGTH_LONG).show()
+        }
     }
 }
