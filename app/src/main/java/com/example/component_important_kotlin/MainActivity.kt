e7ace84267111e519fa9919067b42ef98cd4d5d5
Carlos Maia <carlos.marques89@gmail.com>
2019-12-01 11:04:36 -0500

    Criando um toast customizado

--- app/src/main/java/com/example/component_important_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/component_important_kotlin/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.component_important_kotlin
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.zip.Inflater
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,7 +16,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         buttonToastMe.setOnClickListener {
-            Toast.makeText(this,"Toast Notification",Toast.LENGTH_LONG).show()
+           val toast =  Toast.makeText(this,"Toast Notification",Toast.LENGTH_LONG)
+
+            //Customizando a cor da mensagem dentro do Toast
+            //toast.view.findViewById<TextView>(android.R.id.message).setTextColor(Color.GREEN)
+
+            //Usando o toast customizado (icone e cor)
+            val toastCustom = layoutInflater.inflate(R.layout.toast_custom, null)
+            toast.view = toastCustom
+            val textview = toast.view.findViewById<TextView>(R.id.txtMessage)
+            textview.text = "Toast Customizada"
+
+            toast.show()
         }
     }
 }
